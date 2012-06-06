973f31baf9c0a1a2ea702f9a67d0ee051c69c60a
Dyuman Leonardo Mezzetti <dyuman@lap-dyu.(none)>
2012-06-06 11:33:53 +0530

    Testing with Read_FIFO

--- src/app/Main.c
+++ src/app/Main.c
@@ -68,79 +68,40 @@ extern u8 _vectors[];
 *******************************************************************************/
 int main(void)
 { 
-  /* s32 i;
-     u16 Count_FPS = 0, Second = 0;//,Offset, Result*/ 
-//  u8 N[20];
-//  u8 T_Unit[15]={'u','S','u','S','m','S','S'};
-  
+  u16 i;
+  u32 Batt_Volt;
+  char Batt_Volt_Str[6];
+  u32 FIFO_readout;
+  u32 FIFO_readout_avg;
+  char FIFO_readout_avg_Str[6];
+
   NVIC_SetVectorTable(NVIC_VectTab_FLASH, (u32)&_vectors);
-  
-//Note: 用 IAR_V4.x 编译时，变更 App#n 还要同时修改 lnkarm.xcl 文件中的对应项 
-//      用 IAR_V5.x 编译时，变更 App#n 还要同时修改 xxxxxx.icf 文件中的对应项 
-  
-  /* __USB_Init();*/
-  
-  /* if(__Get(FPGA_OK)== 0){
-    __Display_Str(6*8, 30, YEL, PRN, "      FPGA configuration error       ");
-    while (1){};
-  }
-  
-  __Display_Str(48, 50, WHT, PRN, APP_VERSION);
   Y_Attr = (Y_attr*)__Get(VERTICAL);     
   X_Attr = (X_attr*)__Get(HORIZONTAL);
   G_Attr = (G_attr*)__Get(GLOBAL);
   T_Attr = (T_attr*)__Get(TRIGGER);
-  Load_Attr();                                 // 赋值Y_Attr等
-  i = Load_Param(); 
-  if(i == 0)  // 读取预设开机参数
-    __Display_Str(6*8, 30, GRN, PRN, "     Reload parameter form disk       ");
-  else       
-    __Display_Str(6*8, 30, YEL, PRN, "     Parameter record not found       ");  
-//  i = Load_Param(); // 读取预设开机参数 
+  //  Load_Attr();                                 // 赋值Y_Attr等
 
-*/ /*--------------------------- LICENCE_CTRL_DEMO --------------------------------
-  Offset = Seek_Proj(PROJECT_ID);
-  if(Offset >= 2048){                          // Project ID not found
-    Offset = Seek_Blank();
-    if(Offset == 2048){  
-      __Display_Str(6*8, 50, GRN, PRN,   "         Licence record full         ");
-      while (1){};
-    } else {
-      Result  = Add_Proj(PROJECT_ID, Offset);  // Set project ID
-      Result &= Add_Cnt(DEMOCNT, Offset);      // Set max demo run counter      
-      Result &= Add_Str((u32)PROJECT_STR, Offset); 
-      if(Result != 1){                                
-        __Display_Str(6*8,50,GRN, PRN,   "       Project ID writen error       ");
-        Delayms(500);
-      }
-    }
-  }
-  Licence = Get_Lic(Offset);                   // Get project licence record
-  if(__Ident(DEVELOPER_ID, PROJECT_ID, Licence)!= 1){
-    __Display_Str(6*8, 50, GRN, PRN,   "Please input project licence:00000000");
-    Licence = Input_Lic((6+29)*8, 50);         // Input Licence
-    if(__Ident(DEVELOPER_ID, PROJECT_ID, Licence)!=1)  Result = 0;
-    else{                                      // Licence correct
-      Result  = Add_Lic(Licence, Offset);            
-      if(Result == 1)                                
-        __Display_Str(6*8,50,GRN, PRN, "          Licence writen ok          ");
-      else  
-        __Display_Str(6*8,50,GRN, PRN, "         Licence writen error        ");
-      Delayms(500);
-    }
-    if(Result != 1){
-      __Display_Str(6*8, 30, GRN, PRN, "      Push any key to next step      ");
-      while(Key_Buffer == 0){};
-    }
-  }
-//  --------------------------------------------------------------------------*/
-  
-  /*  Beep_mS = 500;*/
   Balance();
   App_init();
   
   while(1) {
-    //    __Set(BEEP_VOLUME, 50); // Reload volume
+    Batt_Volt = __Get(V_BATTERY); // Get the battery voltage
+    u16ToDec5(Batt_Volt_Str, Batt_Volt);// convert it to a String type
+    __Display_Str(30, 30, PURPL, PRN, Batt_Volt_Str);// Display it
+    __Set(FIFO_CLR, W_PTR);
+    Delayms(20);
+    FIFO_readout_avg = 0;
+    for(i = 0; i < 4096; i++) {
+      FIFO_readout = __Read_FIFO();
+      FIFO_readout = (FIFO_readout & 0xFF); // Ch A
+      FIFO_readout_avg += FIFO_readout;
+    }
+    FIFO_readout_avg = FIFO_readout_avg / 4096;
+    u16ToDec5(FIFO_readout_avg_Str, FIFO_readout_avg);
+    __Display_Str(60, 60, WHT, PRN, FIFO_readout_avg_Str);
+    Delayms(100);
+   //    __Set(BEEP_VOLUME, 50); // Reload volume
     //Beep_mS = 500;                                        // Beep
     if(Key_Buffer != 0) {
       if(Key_Buffer == KEY_P) { // Check if there is any inut
@@ -168,259 +129,4 @@ int main(void)
     __Point_SCR(50,50);
     __LCD_SetPixl(GRN);
   }
-
-  /* Key_Buffer=0;
-  
-//--------------------------------- 主循环 ------------------------------------- 
-  while (1){
-    if(PD_Cnt == 0){
-      __Set(BACKLIGHT, 0);     // 关闭背光
-      __Set(STANDBY, EN);      // 进入省电状态  
-    } else {
-      Synchro();                              // 同步显示各个轨迹波形数据
-      Count_FPS++;
-      if(Second != Sec_Cnt) {
-        Second = Sec_Cnt;
-        Result_FPS = Count_FPS;
-        Count_FPS = 0;
-        Update_Battery();
-        for(i=0; i<9; ++i)  Display_Value(i); // 每秒刷新测量值        
-      }
-      Display_Meter();
-      Display_Title();
-      if(Update){                             // 处理按键后需要刷新的项目
-        Update_Range();
-        Update_Base();
-        Update_Output();
-        Update_Trig();
-        Update_Mark();
-        __Set(BACKLIGHT, 10*(Title[BK_LIGHT][CLASS].Value+1));
-        if(Current != FILE) Update_View_Area();
-        _D_V_Source.Flag |= UPDAT;            // Updat delta V 
-        _Delta_V.Flag    |= UPDAT; 
-        _Delta_T.Flag    |= UPDAT;            // Updat delta T 
-        Update = 0;                           // Update finish
-      } 
-      Blink = 0;
-    }
-    if((_State.Value == HOLD)&&((__Get(FIFO_FULL)!= 0)||(__Get(FIFO_START)== 0))){
-//    if((_State.Value == HOLD)&&(Twink== 1)){
-      _State.Value = 2; 
-      _State.Flag |= UPDAT;
-    }
-//------------------------------ 按键处理循环 ---------------------------------- 
-    if(Key_Buffer) { 
-      if(PD_Cnt == 0)  App_init();          // 退出省电状态
-      PD_Cnt = 600;                         // 600秒
-      if(Key_Buffer == KEY_P){
-        _State.Value = (_State.Value == 0)? 1 : 0;       // "RUN/HOLD" 状态互换
-//        _State.Value = 1-_State.Value;                 // "RUN/HOLD" 状态互换
-        _State.Flag |= UPDAT;                          // 置相应的更新标志
-        if((Current == FILE)&&(_Curr[2].Value == BUF)) reset_parameter();
-        if(_Mode == SGL){
-          for(i=0; i<4*X_SIZE; ++i)  TrackBuff[i] = 0; // 清除旧的显示波形
-          __Set(FIFO_CLR, W_PTR);                      // FIFO写指针复位
-        }
-      }
-      if(Key_Buffer== KEY2){
-        if((Current == TRACK1)||(Current == TRACK2)){
-          Delay_Cnt = 2000;
-          while (Delay_Cnt > 0){
-            if((__Get(KEY_STATUS)& KEY2_STATUS)!=0) break; 
-          }
-          if(Delay_Cnt == 0)  Calibrat(Current);             // 模拟通道校准
-        }
-      }
-      if(Key_Buffer== KEY3){
-        Save_Param();                             // 保存当前操作设置参数    
-        if(Current != FILE){
-          Print_Str(91, 0, 0x0405, PRN, "! Save the current setting !");
-          Delayms(500);
-        } 
-      }
-      if(Key_Buffer== KEY4){
-        if(Current == FILE){
-          Print_Str(230, 0, (SCRN<<8)+ TEXT1, PRN, " >   ");
-        if(_Curr[0].Value == SAVE){
-          switch (_Curr[2].Value){
-          case BMP:
-            i = Save_Bmp(_Curr[1].Value);
-            break;
-          case DAT:
-            i=Save_Dat(_Curr[1].Value);
-            Delayms(1000);
-            break;
-          case BUF:
-            i=Save_Buf(_Curr[1].Value);
-             break;
-          case CSV:
-            i=Save_Csv(_Curr[1].Value);
-            break;
-          }
-          _Curr[1].Value++;
-        } else { 
-          i=0;
-          if(_Curr[2].Value==DAT) i = Load_Dat(_Curr[1].Value);
-          if(_Curr[2].Value==BUF) i = Load_Buf(_Curr[1].Value);
-        }
-        if       (i == OK  ) Print_Str(230, 0, (SCRN<<8)+ TEXT1, PRN, " OK  ");  
-        else{ 
-          if     (i == EMPT) Print_Str(230, 0, (SCRN<<8)+ TEXT1, PRN, " NONE");  
-          else if(i == OVER) Print_Str(230, 0, (SCRN<<8)+ TEXT1, PRN, " FULL");  
-          else               Print_Str(230, 0, (SCRN<<8)+ TEXT1, PRN, " ERR ");  
-        }
-        Delayms(1000);
-        _Curr[1].Flag |= UPDAT; 
-        _Curr[2].Flag |= UPDAT;
-        }  
-      }             
-      if(Key_Buffer == K_ITEM_DEC){ //------------------ "Item-" Key
-        if(Current < METER_0){
-          _Curr[_Det].Flag |= UPDAT;
-          if(Current == TRACK1)  Current = VOLUME;
-          else                   Current --;
-          if(Current == RUNNING) Current --;               // Jump over Item 4
-          if(_Det >2)  _Det =0;
-          _Curr[_Det].Flag |= BLINK;
-          _Curr[0].Flag |= UPDAT;
-          _Curr[1].Flag |= UPDAT;
-          _Curr[2].Flag |= UPDAT;
-          _Curr[3].Flag |= UPDAT;
-        } else {
-          Meter[Current-METER_0].Flag |= UPDAT; 
-          if(Current == METER_0) Current = METER_8;
-          else                   Current --;
-          Meter[Current-METER_0].Flag |= BLINK;
-        }
-      }
-      if(Key_Buffer == K_ITEM_S){ //------------------ "Item_Select"  Key
-        if(Current < METER_0)  Current = METER_0;
-        else                   Current = TRACK1;
-      }
-      if(Key_Buffer == K_ITEM_INC){ //------------------ "Item+" Key
-        if(Current < METER_0){
-          _Curr[_Det].Flag |= UPDAT;
-          if(Current == VOLUME)  Current = TRACK1;
-          else                   Current ++;
-          if(Current == RUNNING) Current ++;              // Jump over RUNNING
-          if(_Det >2)    _Det =0;
-          _Curr[_Det].Flag |= BLINK;
-          _Curr[0].Flag |= UPDAT;
-          _Curr[1].Flag |= UPDAT;
-          _Curr[2].Flag |= UPDAT;
-          _Curr[3].Flag |= UPDAT;
-        } else {
-          Meter[Current-METER_0].Flag |= UPDAT;  
-          if(Current == METER_8) Current  = METER_0;
-          else                   Current ++;
-          Meter[Current-METER_0].Flag |= BLINK;
-        }
-      }
-      if(Key_Buffer == K_INDEX_DEC){ //--------------------- "Index-" Key 
-        if(Current < METER_0){
-          if((Current == TRIGG)&&(Detail[Current]==2)){         // 触发电平调节
-            if(V_Trigg[_Trigg[SOURCE].Value].Value > MIN_Y+4) 
-              V_Trigg[_Trigg[SOURCE].Value].Value--;
-          } else if((Current == BK_LIGHT)||(Current == VOLUME)){// 背光或音量调节
-            if(_Curr[1].Value > 0)   _Curr[1].Value--;
-          } else if((Current == T_BASE)&&(_Det == XPOSI)){      // X_POSI调节
-            if(_Curr[_Det].Value > 30) _Curr[_Det].Value -= 30; 
-            else if(_Curr[_Det].Value > 0) _Curr[_Det].Value--;
-            _X_View.Flag |= UPDAT;                              // 刷新X_View
-          } else {                                              // 当前项为其他
-            if(_Curr[_Det].Value > 0) _Curr[_Det].Value--; 
-            else if(_Curr[_Det].MARK & CIRC) _Curr[_Det].Value =_Curr[_Det].Limit;
-          }
-          if((Current == T_BASE)&&(_Det == MODE)){     // T_BASE MODE 选择
-            Title[RUNNING][STATE].Value = RUN;         // STATE = RUNNING 
-            Title[RUNNING][STATE].Flag |= UPDAT;       // 刷新 RUNNING STATE
-          }
-          if(((Current == OUTPUT)&&(Title[OUTPUT][SOURCE].Value != DIGI))&&
-            (Title[OUTPUT][FRQN].Value > 10)) 
-            Title[OUTPUT][FRQN].Value = 10;            // 模拟信号频率上限为20KHz
-          if((Current == FILE)&&(_Curr[0].Value == LOAD)){ 
-            if(_Curr[2].Value == BMP) _Curr[2].Value = BUF;  // 只能Load Dat,Buf文件
-            if(_Curr[2].Value == CSV) _Curr[2].Value = BUF;  // 只能Load Dat,Buf文件
-          }
-          _Curr[0].Flag |= UPDAT;
-          _Curr[1].Flag |= UPDAT;
-          _Curr[2].Flag |= UPDAT;
-          _Curr[3].Flag |= UPDAT;
-        } else {
-          Meter[Current-METER_0].Flag |= UPDAT;
-          if(Meter[Current-METER_0].Item  > VBT) 
-            Meter[Current-METER_0].Item -= 1;          // 改变测量项目 
-          else                     
-            Meter[Current-METER_0].Item  = TL;//MIN;
-          if(Meter[Current-METER_0].Item == FPS) 
-            Meter[Current-METER_0].Track = 4;
-          if(Meter[Current-METER_0].Item == TL)//MIN) 
-            Meter[Current-METER_0].Track = 0;
-        }
-      }
-      if(Key_Buffer == K_INDEX_S){ //----------------- "Index_Select" Key
-        if(Current < METER_0){                         // 改变Detail
-          _Curr[_Det].Flag |= UPDAT;
-          if(_Det < 3)    _Det += 1;
-          else            _Det  = 0;
-          if(_Curr[_Det].MARK & NOT)                        _Det  = 0; 
-          if(_Curr[_Det].MARK & NUM2)                       _Det  = 0; 
-          if((_Curr[_Det].MARK & NUM3)&&(Current != FILE))  _Det  = 0;  
-          _Curr[_Det].Flag |= BLINK;
-        } else {                                       // 改变测量对象
-          Meter[Current-METER_0].Flag |= UPDAT;
-          if(Meter[Current-METER_0].Track <=  TRACK4) 
-            Meter[Current-METER_0].Track += 1;
-          if(Meter[Current-METER_0].Track > TRACK4) 
-            Meter[Current-METER_0].Track  = TRACK1;
-        }
-      }
-      if(Key_Buffer == K_INDEX_INC){ //--------------------- "Index+" Key 
-        if(Current < METER_0){
-          if((Current == TRIGG)&&(Detail[Current]==2)){         // 触发电平调节
-            if(V_Trigg[_Trigg[SOURCE].Value].Value < MAX_Y-4) 
-              V_Trigg[_Trigg[SOURCE].Value].Value++;
-          } else if((Current == BK_LIGHT)||(Current == VOLUME)){// 背光或音量调节
-            if(_Curr[1].Value < _Curr[1].Limit)   _Curr[1].Value++;
-          } else if((Current == T_BASE)&&(_Det == XPOSI)){      // X_POSI调节
-            if(_Curr[_Det].Value <(_Curr[_Det].Limit-30))  _Curr[_Det].Value += 30; 
-            else if(_Curr[_Det].Value < _Curr[_Det].Limit) _Curr[_Det].Value ++;
-            _X_View.Flag |= UPDAT;                      
-          } else {                                              // 当前项为其他
-            if(_Curr[_Det].Value < _Curr[_Det].Limit)  _Curr[_Det].Value++;
-            else if(_Curr[_Det].MARK & CIRC)   _Curr[_Det].Value  = 0;
-          }
-          if((Current == T_BASE)&&(_Det == MODE)){     // T_BASE MODE 选择
-            Title[RUNNING][STATE].Value = RUN;         // STATE = RUNNING 
-            Title[RUNNING][STATE].Flag |= UPDAT;       // 刷新 RUNNING STATE
-          }
-          if((Current == OUTPUT)&&(Title[OUTPUT][SOURCE].Value != DIGI)){
-            if(Title[OUTPUT][FRQN].Value > 10) 
-              Title[OUTPUT][FRQN].Value = 10;          // 模拟信号频率上限为20KHz
-          }
-          if((Current == FILE)&&(_Curr[0].Value == 1)){  // 只能Load Dat,Buf文件
-            if(_Curr[2].Value == BMP) _Curr[2].Value = DAT;
-            if(_Curr[2].Value == CSV) _Curr[2].Value = DAT;
-          }
-          _Curr[0].Flag |= UPDAT;
-          _Curr[1].Flag |= UPDAT;
-          _Curr[2].Flag |= UPDAT;
-          _Curr[3].Flag |= UPDAT;
-        } else {
-          Meter[Current-METER_0].Flag |= UPDAT;
-          if(Meter[Current-METER_0].Item < TL)//MIN)  
-            Meter[Current-METER_0].Item += 1;          // 改变测量项目
-          else                     
-            Meter[Current-METER_0].Item  = VBT;
-          if(Meter[Current-METER_0].Item == VBT) 
-            Meter[Current-METER_0].Track = 4;
-          if(Meter[Current-METER_0].Item == VPP) 
-            Meter[Current-METER_0].Track = 0;
-        }
-      }
-      Key_Buffer=0;
-      Update = 1;
-    }
-    }*/
 }
-/*********************************  END OF FILE  ******************************/
